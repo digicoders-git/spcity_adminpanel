1c82fbbc4af8d0e92c88b2801268f2885e96ec23
SHIVA VISHWAKARMA <raw66010@gmail.com>
2026-01-17 13:18:57 +0530

    Update API URLs

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,3 @@
-// const API_BASE_URL = 'http://localhost:5000/api';
-// const API_BASE_URL = 'https://sp-city-backend.onrender.com/api';
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://spcity-backend.onrender.com/api';
 
 // Get auth token
